0d41e6f40dfef8cc7ba98dedbc67c6ee66276fbf
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2024-01-26 13:52:16 +0100

    Shop models
    Created tables for shop, categories, products and cart

--- models/models.py
+++ models/models.py
@@ -22,3 +22,41 @@ users = Table(
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
     Column("role_id", Integer, ForeignKey("roles.id")),
 )
+
+
+shops = Table(
+    "shops",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("owner_id", Integer, ForeignKey("users.id"), nullable=False),
+)
+
+categories = Table(
+    "categories",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+)
+
+products = Table(
+    "products",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("description", String),
+    Column("price", Integer, nullable=False),
+    Column("quantity", Integer, nullable=False),
+    Column("shop_id", Integer, ForeignKey("shops.id"), nullable=False),
+    Column("category_id", Integer, ForeignKey("categories.id"), nullable=False),
+)
+
+cart_items = Table(
+    "cart_items",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", Integer, ForeignKey("users.id"), nullable=False),
+    Column("product_id", Integer, ForeignKey("products.id"), nullable=False),
+    Column("quantity", Integer, nullable=False),
+    Column("added_at", TIMESTAMP, default=datetime.utcnow),
+)

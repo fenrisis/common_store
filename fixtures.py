b83fcac7faaa347b3588564c03dc7a3d69e95d94
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2024-01-26 19:42:06 +0100

    Update fixtures.py

--- fixtures.py
+++ fixtures.py
@@ -16,12 +16,27 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-# Insertion
-session.execute(users.insert().values(email='user@example.com', username='user1', password='password', registered_at=datetime.now()))
-session.execute(roles.insert().values(name='Admin', permissions=['create', 'read', 'update', 'delete']))
+# Insert roles
+buyer_role_id = session.execute(roles.insert().values(name='Buyer', permissions=['read'])).inserted_primary_key[0]
 
+# Insert categories and get their IDs
+category_a_id = session.execute(categories.insert().values(name='Category A')).inserted_primary_key[0]
+category_b_id = session.execute(categories.insert().values(name='Category B')).inserted_primary_key[0]
+
+# Ensure that the shop exists before adding products
+shop_id = session.execute(shops.insert().values(name='Shop 1', owner_id=1)).inserted_primary_key[0] # Adding a shop
+
+# Insert user with the buyer role
+buyer_user_id = session.execute(users.insert().values(email='buyer@example.com', username='buyer1', password='password', registered_at=datetime.now(), role_id=buyer_role_id)).inserted_primary_key[0]
+
+# Adding products with existing category_id and shop_id
+product_a_id = session.execute(products.insert().values(name='Product A', description='Description A', price=100, quantity=10, shop_id=shop_id, category_id=category_a_id)).inserted_primary_key[0]
+product_b_id = session.execute(products.insert().values(name='Product B', description='Description B', price=200, quantity=20, shop_id=shop_id, category_id=category_b_id)).inserted_primary_key[0]
+product_c_id = session.execute(products.insert().values(name='Product C', description='Description C', price=300, quantity=30, shop_id=shop_id, category_id=category_a_id)).inserted_primary_key[0]
+
+# Adding products to the cart of the created buyer
+session.execute(cart_items.insert().values(user_id=buyer_user_id, product_id=product_a_id, quantity=2, added_at=datetime.now()))
+session.execute(cart_items.insert().values(user_id=buyer_user_id, product_id=product_c_id, quantity=1, added_at=datetime.now()))
 
 session.commit()
-
-
 session.close()

bf05d7f42eab5a8bc1921a894c5e378d363ec192
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2024-02-10 00:17:14 +0100

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -5,8 +5,8 @@ from email.mime.multipart import MIMEMultipart
 
 
 def send_email(to_email, subject, body):
-    sender_email = "leozaitcevic5431@gmail.com"  # Замените на ваш адрес Gmail
-    sender_password = "a3S-9X>S86/Z8"  # Замените на ваш пароль Gmail
+    sender_email = "******"  # 
+    sender_password = "*********"  # 
 
     message = MIMEMultipart()
     message["From"] = sender_email
@@ -15,9 +15,9 @@ def send_email(to_email, subject, body):
 
     message.attach(MIMEText(body, "plain"))
 
-    # Используйте SMTP сервер Gmail и порт 587 (или 465 для SSL)
+   
     server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()  # Включение шифрования TLS
+    server.starttls()  
     server.login(sender_email, sender_password)
     text = message.as_string()
     server.sendmail(sender_email, to_email, text)
